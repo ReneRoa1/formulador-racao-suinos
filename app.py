f369770275e8534cf59d81c9a81afd968984179a
ReneRoa1 <roa.rene73@gmai.com>
2026-01-28 16:03:53 -0400

    Use Supabase for history

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 from datetime import datetime
 import streamlit.components.v1 as components
 
-from history import save_run, list_runs, load_run
+from history_db import save_run, list_runs, load_run
 from reporting import build_report_html, make_pdf_report
 from io_excel import load_planilha, build_ui_table
 from solver import extract_requirements, solve_lp, calc_dieta, build_results_table
